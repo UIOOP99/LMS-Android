a4b14a4199334149868e6a6a69a60d870fc694f1
hossein.ghojavand.27 <hossein.ghojavand.27@gmail.com>
2020-12-07 15:50:46 +0330

    profile_screen.dart added

--- lib/utils/route_creator.dart
+++ lib/utils/route_creator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lms_app/ui/home-student/home_st_screen.dart';
+import 'package:lms_app/ui/profile_screen.dart';
 
 Route createRouteLoginToHomeSt() {
   return PageRouteBuilder(
@@ -11,6 +12,28 @@ Route createRouteLoginToHomeSt() {
 
       final tween =
           Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
+
+
+Route createRouteHomeStToProfile()
+{
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => ProfileScreen(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(1.0, 0.0);
+      final end = Offset.zero;
+      final curve = Curves.linearToEaseOut;
+
+      final tween =
+      Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
       return SlideTransition(
         position: animation.drive(tween),
